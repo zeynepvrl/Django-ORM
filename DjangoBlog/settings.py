f33e1f68c7fe0dff2e359dbe5f071be3f8bc7562
zeynepvrl <zeynebvural450@gmail.com>
2024-09-08 18:31:51 +0300

    login ve register sayflarına crispy forms filtresi ile bootstrap css lerine erişimleri sağlandı

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'article',           #benim eklediklerim
-    'user'
+    'user',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
